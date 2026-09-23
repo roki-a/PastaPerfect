a3f961627e27243c498c7554c93cf4c90ee880ba
SHANEMENDOZA\mendo <mendozashane0820@gmail.com>
2026-09-23 23:20:07 +0800

    Create Pasta Perfect app shell

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main className="home">
+      <section className="hero">
+        <h1>Pasta Perfect</h1>
+
+        <p>
+          Discover delicious pasta recipes and find your next favorite dish.
+        </p>
+      </section>
+    </main>
+  )
+}
